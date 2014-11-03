500c4e1b8087c7b0023304ae71c57d144e54ac8d
Frank <frank@extri.co>
2014-11-03 10:14:47 +0000

    Removed motd attempt
    Signed-off-by: Frank <frank@extri.co>

--- deploy/attributes/customize.rb
+++ deploy/attributes/customize.rb
@@ -1,6 +1,3 @@
-file 'motd' do
-    content 'A wise cow once said. You got beef mate!?'
-end
 node[:deploy].each do |application, deploy|
     default[:deploy][application][:nodejs][:port] = deploy[:ssl_support] ? 443 : 8081
 end
